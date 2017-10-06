1d88c9b39c9edea5bfeb6a5ae56349201174f264
Gustavo Valdes <Gustavo_Valdes@gap.com>
2017-10-06 03:59:40 -0300

    add basic test

--- src/test/java/com/gustavo/assignment/geocoding/GeocodingApplicationTests.java
+++ src/test/java/com/gustavo/assignment/geocoding/GeocodingApplicationTests.java
@@ -1,17 +1,27 @@
 package com.gustavo.assignment.geocoding;
 
+import org.apache.camel.CamelContext;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GeocodingApplicationTests {
 
+	@Autowired
+	CamelContext camelContext;
 	@Test
 	public void contextLoads() {
+
+		assertNotNull(camelContext.hasEndpoint("direct:processError"));
+		assertNotNull(camelContext.hasEndpoint("direct:googleMapsApi"));
+		assertNotNull(camelContext.hasEndpoint("direct:processServiceError"));
 
 	}
 
